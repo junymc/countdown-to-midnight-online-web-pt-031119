abde8a6dc81a5b4aaf99c773a2db420db4d4957c
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-14 18:00:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(number)
+def countdown(counter)
   counter = 10
   while counter > 0
     counter -= 1
-    puts "#{number} second(s)!"
+    puts "#{counter} second(s)!"
   end
 end
